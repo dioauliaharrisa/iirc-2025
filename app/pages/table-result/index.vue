9670f21f6cb3088880dd3be304952808194a4026
dioauliaharrisa <daharrisa@gmail.com>
2025-11-12 19:54:42 +0700

    'update'

--- app/pages/table-result/index.vue
+++ app/pages/table-result/index.vue
@@ -9,7 +9,6 @@ const parser = new PublicGoogleSheetsParser(
 );
 const items = ref<{ [key: string]: string }[]>([]);
 parser.parse().then((data) => {
-  console.log("🦆 ~ items:", data);
   items.value = data.map((row, index) => ({
     label: `Round ${index + 1}`,
     content: Object.entries(row).map(([key, value]) => ({
@@ -17,40 +16,61 @@ parser.parse().then((data) => {
       value,
     })),
   }));
-  console.log("🦆 ~ items:", items.value);
 });
+const active = ref(["0"]);
 </script>
 
 <template>
-  <UAccordion
-    type="multiple"
-    :items="items"
-    :ui="{
-      item: 'text-white bg-primary border-secondary last:border-b-0',
-    }"
-  >
-    <template #content="{ item }" class="bg-red-500">
-      <div
-        v-for="(entry, i) in item.content"
-        :key="i"
-        class="flex justify-between border-5 border-secondary p-8"
-      >
-        <UAvatarGroup
-          size="xl"
-          :ui="{
-            root: 'transform -rotate-[30deg]',
-            base: 'transform rotate-[30deg]',
-          }"
+  <div class="py-4">
+    <UAccordion
+      v-model="active"
+      type="multiple"
+      :items="items"
+      :ui="{
+        item: 'text-gray-800 bg-gray-100 border-secondary last:border-b-0',
+      }"
+    >
+      <template #content="{ item }">
+        <div
+          v-for="(entry, i) in item.content"
+          :key="i"
+          class="flex justify-start border-1 border-secondary py-8"
         >
-          <UAvatar
-            src="https://github.com/benjamincanac.png"
-            alt="Benjamin Canac"
-          />
-          <UAvatar src="https://github.com/romhml.png" alt="Romain Hamel" />
-          <UAvatar src="https://github.com/noook.png" alt="Neil Richter" />
-          <UAvatar src="https://github.com/noook.png" alt="Neil Richter" />
-        </UAvatarGroup>
-      </div>
-    </template>
-  </UAccordion>
+          <UAvatarGroup
+            size="xl"
+            :ui="{
+              root: ' transform rotate-150',
+              base: 'transform -rotate-150 ring--1 ring-offset-0',
+            }"
+          >
+            <UAvatar
+              src="https://github.com/benjamincanac.png"
+              alt="Benjamin Canac"
+            />
+            <UAvatar src="https://github.com/romhml.png" alt="Romain Hamel" />
+            <UAvatar src="https://github.com/noook.png" alt="Neil Richter" />
+            <UAvatar src="https://github.com/noook.png" alt="Neil Richter" />
+          </UAvatarGroup>
+          <div class="flex -space-x-4 transform rotate-90 ml-6">
+            <div
+              v-for="(score, j) in ['Andy', 'Fai', 'Yi', 'Yo']"
+              :key="j"
+              class="w-10 h-10 flex items-center justify-start text-secondary text-md transform -rotate-90"
+            >
+              {{ score }}
+            </div>
+          </div>
+          <div class="flex -space-x-4 transform rotate-90 ml-6">
+            <div
+              v-for="(score, j) in ['+30.5', '5', '-12', '-40']"
+              :key="j"
+              class="w-10 h-10 flex items-center justify-center text-secondary text-md font-bold transform -rotate-90"
+            >
+              {{ score }}
+            </div>
+          </div>
+        </div>
+      </template>
+    </UAccordion>
+  </div>
 </template>
