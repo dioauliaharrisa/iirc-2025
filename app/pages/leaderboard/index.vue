2ab6a97eeea3f40b7b116cf76d0108427eff7d82
dioauliaharrisa <daharrisa@gmail.com>
2025-11-19 21:19:10 +0700

    'update'

--- app/pages/leaderboard/index.vue
+++ app/pages/leaderboard/index.vue
@@ -46,7 +46,11 @@ const columns = [
               { class: "font-medium text-highlighted" },
               row.original.name
             ),
-            h("p", { class: "" }, `@${row.original.username}`),
+            h(NuxtImg, {
+              src: "https://upload.wikimedia.org/wikipedia/commons/b/b7/Flag_of_Chinese_Taipei_for_Olympic_Games.svg",
+              class: "w-6 h-6 object-cover rounded-md", // adjust size
+              alt: row.original.name,
+            }),
           ]),
         ]
       );
